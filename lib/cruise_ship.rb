a8c10bdcdb65f26fe38f28f73c618b8c3ecc26d4
Eitan Charytan-Spitz <eitands@gmail.com>
2020-09-23 19:08:43 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,5 +7,9 @@
 # }
 
 def select_winner(passengers)
-  # add the code snippet here!
+  winner = ""
+  passengers.each do |room, person|
+    if room == :suite_a && person.start_with?("A")
+      winner = person
+  end
 end
